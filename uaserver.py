dd7c5bcc262aa64389b70535f3f663807842fbc6
cescudero <carlos.esccudero@gmail.com>
2016-01-17 10:40:11 +0100

    uaserver.py

--- uaserver.py
+++ uaserver.py
@@ -5,18 +5,20 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import socket
 import sys
 import os
 import os.path
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import hashlib
 
 try:
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     fichero_audio = sys.argv[3]
 except IndexError:
-    print("Usage: python server.py IP port audio_file")
+    print("Usage: python uaserver.py IP port audio_file")
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -39,8 +41,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
             if Method == 'INVITE':
                 print("El cliente nos manda " + line.decode('utf-8'))
-                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n" + b"SIP/2.0 180 Ring\r\n\r\n" + b"SIP/2.0 200 OK\r\n\r\n")
-
+                respuesta = "SIP/2.0 100 Trying\r\n\r\n" + b"SIP/2.0 180 Ring\r\n\r\n" + b"SIP/2.0 200 OK\r\n\r\n")
+                Content_Type = "Content_type: application/sdp\r\n\r\n"
+                Version = "v=0\r\n" + "o=" + USER + " " + IP + "\r\n"
+                Sesion = "s=misesion\r\n" + "t=0"
+                Audio = "m=audio" + PORT_AUDIO + "RTP\r\n"
+                ENVIAR = respuesta + Version + Sesion + Audio
+                self.wfile.write(bytes(ENVIAR, 
             elif Method == 'ACK':
                 print("El cliente nos manda " + line.decode('utf-8'))
                 aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 <' + fichero_audio
@@ -58,8 +65,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    try:
-        config = sys.argv[1]
+    config = sys.argv[1]
     serv = socketserver.UDPServer((IP, PORT), EchoHandler)
     print("Listening...")
     serv.serve_forever()
